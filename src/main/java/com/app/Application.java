d7825388d1bacb83b9b382a21a6fea6e24837f5b
Balkrishna <balkrishnabhojagude@gmail.com>
2021-05-21 15:06:39 +0530

    HIS-134 changes

--- src/main/java/com/app/Application.java
+++ src/main/java/com/app/Application.java
@@ -10,6 +10,7 @@ public class Application {
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
 		System.out.println("from main");
+		System.out.println("HIS 134");
 		context.refresh();
 		context.close();
 	}
