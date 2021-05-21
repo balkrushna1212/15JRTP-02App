98f47275e0838c7ca5148666f8a4e22e89220b05
Balkrishna <balkrishnabhojagude@gmail.com>
2021-05-21 11:21:45 +0530

    main() modified

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -2,12 +2,16 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
+		System.out.println("from main");
+		context.refresh();
+		context.close();
 	}
 
 }
